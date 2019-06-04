be04da25130dff5d666a67989868306e1c815fdb
Davoleo <davoleo2001@gmail.com>
2019-06-04 17:29:17 +0200

    Fix build file

--- .minecraft/build.bat
+++ .minecraft/build.bat
@@ -1,8 +1,8 @@
 set PACKNAME=Immersivecraft-MK
 set VERSION=1.5.5
 set FILENAME=%PACKNAME%-%VERSION%.zip
-set BUILD_JSON=./general.json
+set BUILD_JSON=./build/general.json
 
-twitch_export-builder.exe -p %VERSION% -n %PACKNAME% -c %BUILD_JSON%
+twitch-export-builder.exe -p %VERSION% -n %PACKNAME% -c %BUILD_JSON%
 move %FILENAME% build/
 pause
